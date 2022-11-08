4f0356ddf994dd86603e0ae37ee4849e1717da05
Bella Bekanova <bellabekanova@Bellas-MacBook-Air.local>
2022-11-08 16:13:59 +0400

    task 1 completed

--- task1.js
+++ task1.js
@@ -1,3 +1,9 @@
 const cats = ["tony", "daisy", "socks", "rockie" ];
 
-const capitalisedCats = // your code here
+const capitalisedCats = cats.map((cat)=> {return cat.toUpperCase()})
+
+function capitaliseWord (item){
+    return item.toUpperCase()
+}
+console.log(capitalisedCats)
+
